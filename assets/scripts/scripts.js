2de3344ceea2442e9f7087d374e5be2a609c361a
davidscdev <davidsc.dev@gmail.com>
2021-10-07 18:07:54 -0300

    Selecionando os itens que aparecem no display para exibir/esconder quando necessário.

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,7 @@
+//Selecionando elementos de tela.
+let telaCabecalho = document.querySelector('.tela--principal--esquerda--titulo span');
+let telaTitulo = document.querySelector('.tela--principal--esquerda--cargo span');
+let telaNumeros = document.querySelector('.tela--principal--esquerda--numeros');
+let telaDados = document.querySelector('.tela--principal--esquerda--dados');
+let telaRodape = document.querySelector('.tela--rodape');
+let telaDireita = document.querySelector('.tela--principal--direita');
