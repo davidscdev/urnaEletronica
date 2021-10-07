76b0033176360797fcb0c3de1673c83b25e90426
davidscdev <davidsc.dev@gmail.com>
2021-10-07 18:21:59 -0300

    Implementada as chamadas das funções de cliques no teclado da urna.

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -5,3 +5,19 @@ let telaNumeros = document.querySelector('.tela--principal--esquerda--numeros');
 let telaDados = document.querySelector('.tela--principal--esquerda--dados');
 let telaRodape = document.querySelector('.tela--rodape');
 let telaDireita = document.querySelector('.tela--principal--direita');
+
+function clicou(n) {
+    console.log('Eleitor clicou: ' + n);
+}
+
+function branco() {
+    console.log('BRANCO CLICADO');
+}
+
+function corrige() {
+    console.log('CORRIGE CLICADO');
+}
+
+function confirma() {
+    console.log('CONFIRMA CLICADO');
+}
